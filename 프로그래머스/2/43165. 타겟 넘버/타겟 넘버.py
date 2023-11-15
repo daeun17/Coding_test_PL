ffa0edba5c9e7566b425cb32faf8d01aff452458
daeun17 <87631575+daeun17@users.noreply.github.com>
2023-11-15 13:35:08 +0900

    [level 2] Title: 타겟 넘버, Time: 5.86 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,22 +1,8 @@
-
+# 모범 답안 
 def solution(numbers, target):
-    answer = 0
-    i = 0
-    answer1 = sum_number(numbers, target - numbers[i], i, len(numbers), answer)
-    answer2 = sum_number(numbers, target + numbers[i], i, len(numbers), answer)
-    # print(answer1 , answer2)
-    return answer1 + answer2
-
-def sum_number (numbers, target, i, end, answer) :
-    
-    # print(numbers[i], i, target,answer)
-    if i == end - 1:
-        # print("end")
-        if target == 0 : 
-            answer += 1
-            # print(answer)
-        return answer
-        
-    answer = sum_number(numbers, target - numbers[i+1], i+1, end, answer)
-    answer = sum_number(numbers, target + numbers[i+1], i+1, end, answer)
-    return answer 
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
