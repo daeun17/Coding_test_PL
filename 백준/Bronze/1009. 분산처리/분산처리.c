5b8f4dc2e57373b348851ef5c4e06885ca455ebe
daeun17 <87631575+daeun17@users.noreply.github.com>
2023-12-11 21:35:04 +0900

    Update 분산처리.c

--- 백준/Bronze/1009. 분산처리/분산처리.c
+++ 백준/Bronze/1009. 분산처리/분산처리.c
@@ -1,21 +1,27 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-int main()
-{
-	int testcase; 
-	scanf("%d", &testcase); // 테스트 케이스 입력 
-	for (int i = 0; i < testcase; i++) // 테스트 케이스에 따른 반복
-	{
-		int a, b;
-		scanf("%d %d", &a, &b); // 데이터 수의 입력
-		int task = a;
-		for (int j = 1; j < b; j++) // 데이터 수의 제곱을 위한 반복문 구현
-		{
-			task = task * a %10;  // 많은 수의 반복문이면 int를 초과할수 있어 나머지 연산 사용
-		}
-		if (task % 10== 0) // 만약 10번째 컴퓨터일경우 
-			printf("%d\n", 10);
-		else //외 
-			printf("%d\n", task % 10);
-	}
+#include <stdio.h>
+
+int main (void){
+    
+    int t;
+    int i, j; 
+    int a , b;
+    int result = 0;
+    scanf("%d", &t);
+
+    for (i = 0; i < t ; i++){
+        scanf("%d %d",&a, &b);
+        result = a;
+
+        for(j = 0 ; j < b - 1; j++){
+            result = result * a % 10; // 오버플로우 방지를 위함, 나머지 값만 있으면 되서 끝자리만 있으면 됨
+        }
+        
+        if(result % 10 == 0){
+            printf("%d\n", 10);
+        }else{
+            printf("%d\n", result % 10); 
+        }
+
+    }
+    return 0; 
 }
