686614b9af1ad742c6461fd55349fd3521a98636
daeun17 <87631575+daeun17@users.noreply.github.com>
2024-06-14 19:34:37 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,7 +1,19 @@
 -- 코드를 입력하세요
-SELECT NAME, DATETIME FROM (
-    SELECT I.NAME, I.DATETIME FROM ANIMAL_INS I , ANIMAL_OUTS O
-    WHERE I.ANIMAL_ID = O.ANIMAL_ID(+)
-    AND O.ANIMAL_ID IS NUll
-    ORDER BY I.DATETIME)
-WHERE ROWNUM <= 3
+SELECT 
+    NAME,
+    DATETIME
+FROM
+    (SELECT 
+        I.NAME, 
+        I.DATETIME
+    FROM 
+        ANIMAL_INS I
+    WHERE 
+        NOT EXISTS (
+            SELECT 1
+            FROM ANIMAL_OUTS O
+            WHERE O.ANIMAL_ID = I.ANIMAL_ID
+        )
+    ORDER BY 
+        I.DATETIME)
+WHERE ROWNUM <= 3;
